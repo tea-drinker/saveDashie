de2c55e2fb669a2a4c45c9422cc24d53f56af00f
Chris Sinclair <chris@quarterstaff.co.uk>
2017-04-04 21:49:38 +0100

    added json parser

--- websocket_utils.py
+++ websocket_utils.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
+import json
+
 class websocket_dummy:
     def recv(self):
-        return '{type:"place", payload:{"x":1, "y":2, "color":0, author:"tea-drinker"}}'
+        return '{"type":"place", "payload":{"x":1, "y":2, "color":0, "author":"tea-drinker"}}'
 
 def websocket_factory():
     return websocket_dummy()
@@ -30,9 +32,16 @@ def websocket_message_processor(next_step_function):
             print('Unknown message:', message)
     return process
 
+def json_message_parser(next_step_function):
+    def process(message):
+        data = json.loads(message)
+        next_step_function(data)
+    return process
+
 if __name__ ==  '__main__':
     #dump debug data from websocket
-    wsp = websocket_message_processor(print)
+    jmp = json_message_parser(print)
+    wsp = websocket_message_processor(jmp)
     wsl = websocket_listener(websocket_factory, wsp)
-    wsl.run()
     
+    wsl.run()
